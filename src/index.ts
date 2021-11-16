e38d0d14005eef4a1bb83859a8602e3285cfa636
daniel <daniel+github@sanitysoln.com>
2021-11-16 19:23:20 +0800

    export locked class

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import SingleRewardProgram from './rewards/SingleRewardProgram'
 import MultiRewardProgram from './rewards/MultiRewardProgram'
+import LockedMultiRewardProgram from './rewards/MultiRewardProgram'
 
-export { SingleRewardProgram, MultiRewardProgram }
+export { SingleRewardProgram, MultiRewardProgram,LockedMultiRewardProgram }
